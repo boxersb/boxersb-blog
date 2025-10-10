03dd01791d81a9d98e4fe50275694f47b7714e05
Kyoungtaek Koo <kyoungtaek.koo@navercorp.com>
2025-10-10 22:31:56 +0900

    :package: Add code-style packages

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import naverpay from "@naverpay/eslint-config";
+
+export default [
+  {
+    ignores: ["**/dist/**"],
+  },
+  ...naverpay.configs.node,
+  ...naverpay.configs.typescript,
+  ...naverpay.configs.strict,
+  ...naverpay.configs.packageJson,
+];
